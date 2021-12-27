4f32957d929aed77a391680d1c6b211c4d52ebc9
Filizzola <juliofilizzola@hotmail.com>
2021-12-27 10:13:34 -0300

    🍱: movendo order para class persistency

--- src/classes/order.ts
+++ src/classes/order.ts
@@ -1,11 +1,16 @@
 import { OrderStatus } from "./interfaces/order-status";
 import { Messaging } from "./messaging";
+import { Persistency } from "./persistency";
 import { ShoppingCart } from "./ShoppingCart";
 
 export class order {
   private _orderStatus: OrderStatus = 'open';
 
-  constructor( private readonly cart: ShoppingCart, private readonly msg: Messaging) {}
+  constructor(
+    private readonly cart: ShoppingCart, 
+    private readonly msg: Messaging,
+    private readonly saveOrder: Persistency
+  ) {}
 
   get orderStatus(): OrderStatus {
     return this._orderStatus;
@@ -18,13 +23,8 @@ export class order {
     }
     this.msg.sendMessage(`seu pedido com total de ${this.cart.total()} foi recebido`);
     
-    this.saveOrder();
+    this.saveOrder.saveOrder();
     this.cart.clear();
-  }
-
-
-  saveOrder(): void {
-    console.log('Pedido salvo com sucesso!');
   }
 
 }
