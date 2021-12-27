f84c67047e3027da73eb98e6e1f46e5441e55069
Filizzola <juliofilizzola@hotmail.com>
2021-12-27 11:38:30 -0300

    🔧: imprimindo totalWithDiscount

--- src/ocp/classes/main.ts
+++ src/ocp/classes/main.ts
@@ -16,6 +16,7 @@ shoppingCart.addItem(new Product('Caderno', 9.9123));
 shoppingCart.addItem(new Product('Lápis', 1.59));
 
 console.log(shoppingCart.items);
+console.log(shoppingCart.totalWithDiscount());
 console.log(shoppingCart.total());
 console.log(order.orderStatus);
 order.checkout();
