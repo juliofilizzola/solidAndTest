068e75d63640a9c281547c6bd850f0955b5dde2c
Filizzola <juliofilizzola@hotmail.com>
2021-12-27 11:48:53 -0300

    :wrench:: editnado method totalWithDiscount

--- src/ocp/classes/main.ts
+++ src/ocp/classes/main.ts
@@ -16,7 +16,7 @@ shoppingCart.addItem(new Product('Caderno', 9.9123));
 shoppingCart.addItem(new Product('Lápis', 1.59));
 
 console.log(shoppingCart.items);
-console.log(shoppingCart.totalWithDiscount());
+console.log(shoppingCart.totalWithDiscount(0.5));
 console.log(shoppingCart.total());
 console.log(order.orderStatus);
 order.checkout();
