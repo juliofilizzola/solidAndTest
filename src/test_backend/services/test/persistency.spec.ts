b2b3468388c5e3d6e7febf79bcf602b7beba09ca
Filizzola <juliofilizzola@hotmail.com>
2021-12-29 14:28:28 -0300

    📝: add comentarios nos test

--- src/test_backend/services/test/persistency.spec.ts
+++ src/test_backend/services/test/persistency.spec.ts
@@ -1,15 +1,25 @@
 import { Persistency } from "../persistency";
 
 describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks()); // limpando os mocks a cada test. 
+
   it('should return undefined', () => {
-    const swt = new Persistency(); // swt = system under test;
-    expect(swt.saveOrder()).toBeUndefined();
+    const sut = new Persistency(); // sut = system under test;
+    expect(sut.saveOrder()).toBeUndefined();
   });
 
   it('should call console.log once', () => {
-    const swt = new Persistency();
-    const consoleSpy = jest.spyOn(console, 'log');
-    swt.saveOrder();
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log'); // fica espiando o console se ele é chamado o log.
+    sut.saveOrder();
     expect(consoleSpy).toHaveBeenCalledTimes(1);
-  })
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso!"', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso!');
+  });
+
 })
