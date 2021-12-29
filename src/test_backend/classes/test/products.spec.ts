9e09e353444f1ee8e216c5d04df2e8e6ddbf9269
Filizzola <juliofilizzola@hotmail.com>
2021-12-29 15:08:53 -0300

    :white_check_mark:: criando test para o product

---
+++ src/test_backend/classes/test/products.spec.ts
@@ -0,0 +1,16 @@
+import { Product } from '../products';
+
+const createSUT = (name: string, price: number): Product => {
+  return new Product(name, price);
+}
+
+describe('Products test', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefind', () => { 
+    const sut = createSUT('camisa', 40);
+    expect(sut).toHaveProperty('name', 'camisa');
+    expect(sut).toHaveProperty('price', 40);
+    expect(sut).not.toHaveProperty('name', 40);
+  });
+})
