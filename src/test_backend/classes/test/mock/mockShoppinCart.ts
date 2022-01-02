5329244c1574cce2dee7a1d31437f15eb1440311
Filizzola <juliofilizzola@hotmail.com>
2022-01-02 19:13:20 -0300

    ✅: test com inserção de itens no carrinho finalizado

--- src/test_backend/classes/test/mock/mockShoppinCart.ts
+++ src/test_backend/classes/test/mock/mockShoppinCart.ts
@@ -7,3 +7,13 @@ export class DiscountMock extends Discount {};
 export class CartItemMock implements CartItem {
   constructor(public name: string, public price: number) {}
 };
+
+export const productMock1 = {
+  name: "Chinelo Havaina",
+  price: 15.00
+};
+
+export const productMock2 = {
+  name: "mousepad",
+  price: 58.00
+}
--- src/test_backend/classes/test/shopping-cart.spec.ts
+++ src/test_backend/classes/test/shopping-cart.spec.ts
@@ -1,6 +1,6 @@
 import { CartItem } from '../interfaces/cart-item';
 import { ShoppingCart } from '../ShoppingCart';
-import { DiscountMock, CartItemMock } from './mock/mockShoppinCart';
+import { DiscountMock, CartItemMock, productMock1, productMock2 } from './mock/mockShoppinCart';
  
 const createSUT = (): ShoppingCart => {
   return new ShoppingCart(new DiscountMock());
@@ -20,6 +20,10 @@ describe('shopping cart test', () => {
 
   it('should have 2 cart items', () => {
     const sut = createSUT();
+    const product1 = createItem(productMock1.name, productMock1.price);
+    const product2 = createItem(productMock2.name, productMock2.price);
+    sut.addItem(product1);
+    sut.addItem(product2);
     expect(sut.items.length).toBe(2);
   });
 });
