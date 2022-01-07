e53eb9576886ff327f318eba6d06943e2802e1e0
Filizzola <juliofilizzola@hotmail.com>
2022-01-06 21:48:26 -0300

    ⚡️: finalizando teste do order

--- src/test_backend/classes/test/order.spec.ts
+++ src/test_backend/classes/test/order.spec.ts
@@ -1,50 +1,105 @@
 import { CartItem } from '../interfaces/cart-item';
+import { CustomerOrder } from '../interfaces/customer';
 import { IMessaging } from '../interfaces/Messaging';
 import { IPersistency } from '../interfaces/persistency';
 import { IShoppingCart } from '../interfaces/Shopping-cart';
 import { Order } from '../order';
-class shoppingCartMock implements IShoppingCart {
+class ShoppingCartMock implements IShoppingCart {
   get items(): readonly CartItem[] {
     return [];
   }
-  addItem(item: CartItem): void {
-    throw new Error('Method not implemented.');
-  }
-  removeItem(index: number): void {
-    throw new Error('Method not implemented.');
-  }
+
+  addItem(item: CartItem): void {}
+
+  removeItem(index: number): void {}
+
   total(): number {
-    throw new Error('Method not implemented.');
+   return 1;  
   }
+
   totalWithDiscount(): number {
-    throw new Error('Method not implemented.');
+    return 1;
   }
+
   isEmpty(): boolean {
-    throw new Error('Method not implemented.');
+    return true;
   }
-  clear(): void {
-    throw new Error('Method not implemented.');
-  }
+
+  clear(): void {}
 
 }
 
 class MessingMock implements IMessaging {
-  sendMessage(msg: string): void {
-    throw new Error('Method not implemented.');
-  }
+  sendMessage(msg: string): void {}
 }
 
 class PersistencyMock implements IPersistency {
-  saveOrder(): void {
-    throw new Error('Method not implemented.');
+  saveOrder(): void {}
+}
+
+class CustomerMock implements CustomerOrder {
+  getName(): string {
+    return '';
+  }
+  getIDN(): string {
+    return '';
+  }
+
+}
+
+const makeSut = ()  => {
+  const shoppingCartMock = new ShoppingCartMock();
+  const messagingMock = new MessingMock();
+  const persistencyMock = new PersistencyMock();
+  const customerMock = new CustomerMock();
+  const sut = new Order(shoppingCartMock, messagingMock, persistencyMock, customerMock);
+
+  return { 
+    shoppingCartMock,
+    messagingMock,
+    persistencyMock,
+    customerMock,
+    sut
   }
 }
 
-
-
 describe("Order", () => {
+  afterAll(() => jest.clearAllMocks());
+
   it('should not checkout if cart is empty', () => {
+    const { sut, shoppingCartMock } = makeSut();
+    const shoppingCartSpy = jest.spyOn(shoppingCartMock, 'isEmpty').mockReturnValueOnce(true);
+    sut.checkout();
+    expect(shoppingCartSpy).toHaveBeenCalledTimes(1);
+    expect(sut.orderStatus).toBe("open");
+  });
 
+  it('should checkout if cart is not empty', () => {
+    const { sut, shoppingCartMock } = makeSut();
+    const shoppingCartSpy = jest.spyOn(shoppingCartMock, 'isEmpty').mockReturnValueOnce(false);
+    sut.checkout();
+    expect(shoppingCartSpy).toHaveBeenCalledTimes(1);
+    expect(sut.orderStatus).toBe("closed");
+  });
 
+  it('should send an email to customer', () => {
+    const { sut, messagingMock } = makeSut();
+    const messagingMockSpy = jest.spyOn(messagingMock, 'sendMessage');
+    sut.checkout();
+    expect(messagingMockSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should save order', () => {
+    const { sut, persistencyMock } = makeSut();
+    const persistencyMockSpy = jest.spyOn(persistencyMock, 'saveOrder');
+    sut.checkout();
+    expect(persistencyMockSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should clear cart', () => {
+    const { sut, shoppingCartMock } = makeSut();
+    const shoppingCartMockSpy = jest.spyOn(shoppingCartMock, 'clear');
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
   });
 });
